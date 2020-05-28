209cdd84e54d2cc18a2d8cf7fcf7e8aab5613055
Kyle PC <kyle.dahl201@gmail.com>
2020-05-28 04:55:00 -0500

    adding get function for notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,18 @@
 const router = require("exrpess").Router();
 const store = require("../db/store");
 
+//get function to get the notes 
+
+router.get("/notes", function(req, res) {
+    store
+        .getNotes()
+        .then(notes => res.json(notes))
+        .catch(err => res.status(500).json(err));
+
+
+});
+
+
+
+//post route function 
+
